55638a1e0a1d0108175aec4311b4aae36eee59a1
Justin <nebojustin@gmail.com>
2017-10-23 16:10:43 +0100

    add function source file

---
+++ source/index.js
@@ -0,0 +1,38 @@
+const primeFactor = (number) => {
+    for (let i = 2; i <= 7; i++) {
+        if (number % i === 0) {
+            return i;
+        }
+    }
+    return number;
+};
+
+const rainDrops = (number) => {
+    let numberToTest = number,
+        factor = undefined,
+        primeFactors = [];
+
+    if (typeof number !== 'number') {
+        return 'Invalid input'
+    }
+    else {
+        if (number < 3) {
+            return number;
+        }
+
+        while (numberToTest !== 1) {
+            factor = primeFactor(numberToTest);
+            primeFactors.push(factor);
+            numberToTest /= factor;
+        }
+    }
+    return primeFactors.includes(3) && primeFactors.includes(5) && primeFactors.includes(7) ? 'PlingPlangPlong' :
+        primeFactors.includes(3) && primeFactors.includes(5) ? 'PlingPlang' :
+            primeFactors.includes(3) && primeFactors.includes(7) ? 'PlingPlong' :
+                primeFactors.includes(3) ? 'Pling' :
+                    primeFactors.includes(5) && primeFactors.includes(7) ? 'PlangPlong' :
+                        primeFactors.includes(5) ? 'Plang' :
+                            primeFactors.includes(7) ? 'Plong' : number;
+}
+
+export default rainDrops;
